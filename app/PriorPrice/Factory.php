670754261fa7595b0348ac04a6e3d009bad52a83
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2022-12-27 20:39:21 +0100

    removed shortcodes class

--- app/PriorPrice/Factory.php
+++ app/PriorPrice/Factory.php
@@ -10,32 +10,11 @@ namespace PriorPrice;
 class Factory {
 
 	/**
-	 * @since 1.0
-	 * 
-	 * @var Shortcode|null
-	 */
-	private static $shortcode;
-
-	/**
 	 * $since 1.0
 	 *
 	 * @var \PriorPrice\Revisions|null;
 	 */
 	private static $revisions;
-
-	/**
-	 * Create Shortcode instance.
-	 * 
-	 * @since 1.0
-	 * 
-	 * @return Shortcode
-	 */
-	public function createShortcode() {
-		if ( ! self::$shortcode ) {
-			self::$shortcode = new Shortcode();
-		}
-		return self::$shortcode;
-	}
 
 	public function createRevisions() {
 		if ( ! self::$revisions ) {
