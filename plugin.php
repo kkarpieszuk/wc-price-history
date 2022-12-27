5eac89a8dbbea163de32f7dcb2166cbfd5901cc1
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2022-12-27 17:07:19 +0100

    [fixup] rename

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Plugin Name: Prior Price
- * Description: Track Woocommerce Products prices history and display the lowest price in the last 30 days. This plugin allows your WC shop to be compliant with European Commission Directive 98/6/EC Article 6a which specifies price reduction announcement policy.
+ * Plugin Name: Previous Prices
+ * Description: Track Woocommerce Products prior prices history and display the lowest price in the last 30 days. This plugin allows your WC shop to be compliant with European Commission Directive 98/6/EC Article 6a which specifies price reduction announcement policy.
  * Author: Konrad Karpieszuk
  * Version: 1.0
  */
