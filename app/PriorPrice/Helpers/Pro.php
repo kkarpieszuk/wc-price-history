c8a2eadc5b47a8e622b927bd6e266c054f9e69ab
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2025-08-27 07:09:53 +0200

    phpdoc

--- app/PriorPrice/Helpers/Pro.php
+++ app/PriorPrice/Helpers/Pro.php
@@ -2,8 +2,20 @@
 
 namespace PriorPrice\Helpers;
 
+/**
+ * Pro helper.
+ *
+ * @since {VERSION}
+ */
 class Pro {
 
+	/**
+	 * Check if the plugin is pro.
+	 *
+	 * @since {VERSION}
+	 *
+	 * @return bool
+	 */
 	public static function is_pro() : bool {
 		return defined( 'WC_PRICE_HISTORY_EDITOR_PLUGIN_URL' );
 	}
