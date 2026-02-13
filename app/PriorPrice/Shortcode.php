9348a0254b1b52b3492a7cbefc657c7b13039d48
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2026-02-13 09:18:27 +0100

    replace version

--- app/PriorPrice/Shortcode.php
+++ app/PriorPrice/Shortcode.php
@@ -149,7 +149,7 @@ class Shortcode {
 	 *
 	 * Renders placeholder text and a hidden price block so frontend JS can show the price on found_variation.
 	 *
-	 * @since {VERSION}
+	 * @since 3.2.1
 	 *
 	 * @param \WC_Product_Variable       $product Product.
 	 * @param array<string, int|string|null> $atts   Shortcode attributes (from shortcode_atts).
